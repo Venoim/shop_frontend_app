53f4e8cb3ab98b67be7943dc46c96c812ab21075
Venoim <ascholz47@gmail.com>
2024-02-21 17:32:08 +0100

    move menu page to folders

--- src/pages/User/UserPage.js
+++ src/pages/User/UserPage.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import UserSidebar from "../../components/Sidebar/UserSidebar.js";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import OrdersPage from "../../pages/User/UserMenu/OrdersPage.js";
-import DataPage from "../../pages/User/UserMenu/DataPage.js";
+import OrdersPage from "./Orders/OrdersPage.js";
+import DataPage from "./Data/DataPage.js";
 // import AccountSettingsPage from "./UserMenu/AccountSettingsPage.js";
 import "bulma/css/bulma.min.css";
 import "./UserPageStyle.scss";
